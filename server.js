1c883e8bd9c9fd5f700b614813957b65c1df0d12
Dusty <dustygaron@hotmail.com>
2020-01-29 12:03:01 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+
+const app = express()
+require('dotenv').config()
+
+// DB Config
+const db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+// Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`🏃‍♂️ 🏃‍♂️ 🏃‍♂️ Server running on port ${port}`))
