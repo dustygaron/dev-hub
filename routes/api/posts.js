4fddefb35340c6344f755e51d34a09463a8fd6a4
Dusty <dustygaron@hotmail.com>
2020-02-03 10:34:59 -0500

    Get and delete postroutes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,11 +7,11 @@ const Post = require('../../models/Post')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
 
-// ---------------------
+// -----------------------
 // @route   POST api/posts
 // @desc    Create a post
 // @access  Private
-// ---------------------
+// -----------------------
 router.post('/', [auth, [
   check('text', 'Text is required').not().isEmpty()
 ]],
@@ -41,6 +41,88 @@ router.post('/', [auth, [
     }
   }
 )
+
+
+// -----------------------
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+// -----------------------
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 })
+    res.json(posts)
+  } catch (err) {
+    console.log(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+
+// -----------------------
+// @route   GET api/posts/:id
+// @desc    Get posts by ID
+// @access  Private
+// -----------------------
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).sort({ date: -1 })
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    s
+    res.json(post)
+
+  } catch (err) {
+    console.log(err.message)
+
+    // If user ID is not valid
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    res.status(500).send('Server Error')
+  }
+})
+
+
+// -----------------------
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+// -----------------------
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // If post not found
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    // Check user auth -- req.user.id is the logged in user
+    // post.user is Object & req.user.id is String
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
+
+    await post.remove()
+    res.json({ msg: 'Post removed' })
+
+  } catch (err) {
+    console.log(err.message)
+
+    // If user ID is not valid
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    res.status(500).send('Server Error')
+  }
+})
+
+
 
 
 module.exports = router
