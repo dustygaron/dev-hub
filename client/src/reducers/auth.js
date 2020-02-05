e7ce715e8c7e893c44fe889264717f8206116e43
Dusty <dustygaron@hotmail.com>
2020-02-05 13:08:33 -0500

    User login

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -6,7 +6,9 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from '../actions/types'
 
 const initialState = {
@@ -29,6 +31,7 @@ export default function (state = initialState, action) {
         user: payload
       }
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token)
       return {
         ...state,
@@ -38,6 +41,7 @@ export default function (state = initialState, action) {
       }
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem('token')
       return {
         ...state,
