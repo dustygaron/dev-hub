00e936bba8f519e65668cf9954da3a21d62087df
Dusty <dustygaron@hotmail.com>
2020-02-05 20:52:32 -0500

    Create profile action

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
+import {
+  // Link,
+  withRouter
+} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { createProfile } from '../../actions/profile'
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
 
   const [formData, setFormData] = useState({
     company: '',
@@ -40,18 +45,22 @@ const CreateProfile = props => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
+  const onSubmit = e => {
+    e.preventDefault()
+    createProfile(formData, history)
+  }
 
   return (
     <>
-      <h1 class="large text-primary">
+      <h1 className="large text-primary">
         Create Your Profile
       </h1>
-      <p class="lead">
-        <i class="fas fa-user"></i> Let's get some information to make your
+      <p className="lead">
+        <i className="fas fa-user"></i> Let's get some information to make your
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -209,7 +218,7 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+  createProfile: PropTypes.func.isRequired
 }
 
-export default CreateProfile
+export default connect(null, { createProfile })(withRouter(CreateProfile))
