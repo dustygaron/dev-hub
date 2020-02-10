4101e7ed0f5158d46666c2bd58996f6cb7dfc6b7
Dusty Garon <dustygaron@Dustys-iMac.local>
2020-02-10 13:38:37 -0500

    Profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -14,8 +14,12 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
   return (
     <>
       {profile === null || loading ? <Spinner /> : <>
-        <Link to='/profiles' className='btn btn-light'>Back to Profiles</Link>
-        {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && <Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>}
+        <Link to='/profiles' className='btn btn-light'>
+          Back to Profiles
+        </Link>
+        {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && <Link to='/edit-profile' className='btn btn-dark'>
+          Edit Profile
+        </Link>}
       </>}
     </>
   )
