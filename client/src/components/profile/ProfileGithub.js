6a2bdcec1c5a7fbc04459d0df82a7c22231ae884
Dusty Garon <hello@dustygaron.com>
2020-02-11 15:03:59 -0500

    Displaying github repos

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -18,6 +18,7 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
       ) : (
           repos.map(repo => (
             <div key={repo.id} className="repo bg-white p-1 my-1">
+
               <div>
                 <h4>
                   <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
