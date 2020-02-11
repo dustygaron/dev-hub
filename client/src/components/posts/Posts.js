9587cace3d64f508ab95a7b034fcc9a60c5e7bee
Dusty Garon <hello@dustygaron.com>
2020-02-11 17:47:54 -0500

    Post item

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,20 +2,32 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getPosts } from '../../actions/post'
+import PostItem from './PostItem'
+import Spinner from '../layouts/Spinner'
 
-
-const Posts = ({ getPosts, post: { post, loading } }) => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
 
   useEffect(() => {
     getPosts()
   }, [getPosts])
 
   return (
-    <div>
-
-    </div>
+    loading ? <Spinner /> :
+      <>
+        <h1 className="large text-primary">Posts</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Welcome to the Community
+        </p>
+        <div className="posts">
+          {posts.map(post => (
+            <PostItem key={post._id} post={post} />
+          ))}
+        </div>
+      </>
   )
+
 }
+
 
 Posts.propTypes = {
   getPosts: PropTypes.func.isRequired,
