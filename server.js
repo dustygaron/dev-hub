1b0bc89a9ff3e0b16d8a42cb818a797016df9170
Dusty Garon <hello@dustygaron.com>
2020-02-12 18:35:22 -0500

    Connect heroku

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ connectDB()
 // Init Middleware - Allows us to get data from req.body
 app.use(express.json({ extended: false }))
 
-
-
 // Routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
