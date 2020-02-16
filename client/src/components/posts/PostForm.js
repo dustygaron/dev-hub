3b701681b141299e69beb7b0ba2d377b54d69988
Dusty Garon <hello@dustygaron.com>
2020-02-16 14:01:37 -0500

    Yet more styling

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -8,28 +8,28 @@ const PostForm = ({ addPost }) => {
   const [text, setText] = useState('')
 
   return (
-    <div className='post-form card'>
-      <div className='bg-primary p'>
+    <div className="post-form">
+      <div className="bg-primary p">
         <h3>Say Something...</h3>
       </div>
 
       <form
-        className='form my-1'
+        className="form my-1"
         onSubmit={e => {
           e.preventDefault()
           addPost({ text })
           setText('')
         }}>
         <textarea
-          name='text'
-          cols='30'
-          rows='5'
-          placeholder='Create a post'
+          name="text"
+          cols="30"
+          rows="5"
+          placeholder="Create a post"
           value={text}
           required
           onChange={e => setText(e.target.value)}
         ></textarea>
-        <input type='submit' className='btn btn-dark my-1' value='Submit' />
+        <input type="submit" className="btn btn-dark my-1" value="Submit" />
       </form>
 
     </div>
